0d1481463adc4ad641581eb7f9620e867704195d
qinyueshang <yue_hong_y@163.com>
2017-08-27 12:28:38 +0800

    midified

--- nodePaChong/myPaChong/index.js
+++ nodePaChong/myPaChong/index.js
@@ -2,7 +2,7 @@
 const requestLagou=require('./getData');
 
 let city=["北京","上海","广州","深圳","杭州","南京","成都","西安","武汉","重庆"];
-let position=["web前端","java","php","ios","android","c++","python",".NET"];
+let position=["web","java","php","ios","android","c++","python",".NET"];
 
 if(process.argv.length === 4&&city.includes(process.argv[2])&&position.includes(process.argv[3])){
   console.log('准备开始请求' + process.argv[2] + '的' + process.argv[3] + '职位数据');
@@ -10,5 +10,5 @@ if(process.argv.length === 4&&city.includes(process.argv[2])&&position.includes(
 }else{
   console.log('请正确输入要爬取的城市和职位,正确格式为 node index 城市 职位，如 node index 上海 前端')
   console.log('城市如下：北京,上海,广州,深圳,杭州,南京,成都,西安,武汉,重庆')
-  console.log('职位如下：web前端,java,php,ios,android,c++,python,.NET')
+  console.log('职位如下：web,java,php,ios,android,c++,python,.NET')
 }
